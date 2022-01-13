c8762039d8b3376cfe8bf009d0525390fc8bc1dd
Tom L'Hermenier <tomlhermenier@gmail.com>
2022-01-13 15:23:26 +0100

    appel vers api rest pour les vols + préparation du formulaire des hotels

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 let baliseVols = document.getElementById('vols')
 let baliseChambres = document.getElementById('chambres')
 
-getJSON("http://localhost:8080/java_restserver_war_exploded/api/compagnies/1/vols/17/places/17")
-console.log
+let submitHotelForm = document.getElementById('hotelForm')
+let dateArrive = document.getElementById('dateArrive')
+let dateDepart = document.getElementById('dateDepart')
+
+init()
+
+function init() {
+    getJSON("http://localhost:8080/java_restserver_war_exploded/api/compagnies/1/vols/17/places")
+    initForm()
+}
+
+function initForm() {
+    console.log('init form')
+    dateDepart.setAttribute('disabled', 'disabled')
+    dateArrive.addEventListener('change', function() {
+        if(dateArrive.value != "") {
+            dateDepart.removeAttribute('disabled')
+            dateDepart.setAttribute('min', dateArrive.value)
+        }
+        else {
+            dateDepart.setAttribute('disabled', 'disabled')
+        }
+    })
+
+    submitHotelForm.addEventListener('submit', function(event) {
+        event.preventDefault()
+        
+        console.log(dateArrive.value)
+        console.log(dateDepart.value)
+    })
+}
 
 function getJSON(url) {
     let headers = new Headers();
@@ -18,9 +47,14 @@ function getJSON(url) {
     .then(function(response) {
         response.json().then(function(response) {
             console.log(response)
-            //todo affichage
+            response.forEach(element => {
+                let button = document.createElement('a')
+                button.innerHTML = element.numero_place
+                button.className = element.available ? 'btn btn-success' : 'btn btn-dark disabled'
+                button.setAttribute('role', 'button')
+                baliseVols.appendChild(button)
+            });
         } 
     )})
     .catch(err => console.log(err))
-
 }
