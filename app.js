bcf4678acf8b2ad9a1350bb2a6ff2762ddb523b2
Tom L'Hermenier <tomlhermenier@gmail.com>
2022-01-21 11:32:19 +0100

    updata app

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ function initForm(isSecurised) {
             alert("Veuillez sélectionner une chambre")
         }
         else {
-        let url = "http://localhost:8090/" + serverName + "/api/" + isSecurised + "rooms/" + idSelectedRoom 
+        let url = "http://localhost:8090/" + serverName + "/api/" + isSecurised + "/" + "rooms/" + idSelectedRoom
         fetch(url, {
             method: 'POST',
             headers: headers,
@@ -81,17 +81,18 @@ function initPlaces(url) {
         response.json().then(function(response) {
             response.forEach(element => {
                 let button = document.createElement('a')
-                button.innerHTML = element.numero_place
+                button.innerHTML = element.numPlace
                 button.className = element.available ? 'btn btn-success' : 'btn btn-dark disabled'
                 button.setAttribute('role', 'button')
                 if(element.available) {
                     button.addEventListener('click', function(event) {
-                        fetch(url + "/" + element.numero_place, {
+                        fetch(url + "/" + element.numPlace, {
                             method: 'POST',
                             headers: headers
                         })
                         .then(function(response) {
                             response.json().then(function(response) {
+                                button.className = response ? 'btn btn-dark disabled' : 'btn btn-warning disabled'
                                 response ? alert('Réservation réussis') : alert('Impossible de réserver cette place')
                                 location.reload()
                             })
@@ -115,11 +116,11 @@ function initChambres(url) {
             let i = 1
             response.forEach(element => {
                 let button = document.createElement('a')
-                button.innerHTML = element.room_number
+                button.innerHTML = element.roomNumber
                 button.className = 'btn btn-success'
                 button.id = i
                 button.setAttribute('role', 'button')
-                onClickFirst[i] = listenerFirstClick.bind(null, element.room_number)
+                onClickFirst[i] = listenerFirstClick.bind(null, element.roomNumber)
                 button.addEventListener('click', onClickFirst[i])
                 chambres.appendChild(button)
                 i++
